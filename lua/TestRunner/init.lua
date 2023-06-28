6629fb116884eb3b0a11cc28da96f47518ec3969
Milos Zivlak <zivlakmilos@gmail.com>
2023-06-28 02:29:33 +0200

    Cleanup config

--- lua/TestRunner/init.lua
+++ lua/TestRunner/init.lua
@@ -4,9 +4,6 @@ function M.setup(config)
 	if not config then
 		config = {}
 	end
-	local curDir = vim.fn.getcwd()
-	print("Hello World!")
-	print(curDir)
 end
 
 function M.run_test()
@@ -14,9 +11,9 @@ function M.run_test()
 	local command = nil
 
 	if extension == "lua" then
-		command = 'call jobsend(b:terminal_job_id, "")'
-	elseif extension == "js" or extension == "ts" or extension == "jsx" or extension == "tsx" then
 		command = "call jobsend(b:terminal_job_id, \"nvim --headless -c 'PlenaryBustedDirectory tests'\")"
+	elseif extension == "js" or extension == "ts" or extension == "jsx" or extension == "tsx" then
+		command = 'call jobsend(b:terminal_job_id, "npm run test")'
 	end
 
 	if command then
